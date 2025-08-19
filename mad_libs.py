5122cbcada56ab2168f3ff3de233d6708435a503
dsjambekar <dsjambekar@gmail.com>
2025-08-19 09:02:10 -0400

    very basic mad libs project

---
+++ mad_libs.py
@@ -0,0 +1,6 @@
+adj = input("Enter an adjective: ");
+noun1 = input("Enter a noun: ");
+noun2 = input("Enter a noun: ");
+sentence = f"The {adj} superhero saved the {noun1} from the {noun2}."
+
+print(sentence);
